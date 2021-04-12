fe650db02d928a7168bdc75b16b494f4fc0d5b87
andrewnijmeh <andrewnijmeh1@gmail.com>
2021-04-12 12:33:13 -0400

    refactor 505b

--- data/__init__.py
+++ data/__init__.py
@@ -7,7 +7,7 @@ BASEDIR = pathlib.Path(__file__).parent.absolute()
 with open(os.path.join(BASEDIR, "rcsb_pdb_505B.fasta")) as f:
   picornaviridae = ''.json(f.read().split("\n")[1:]).upper()
 
-def get_amber99sb():
+def get_505b():
   from xml.dom import minidom
-  amber99sb = minidom.parse(os.path.join(BASEDIR, "505b.xml"))
-  return amber99sb
+  505b = minidom.parse(os.path.join(BASEDIR, "505b.xml"))
+  return 505b
