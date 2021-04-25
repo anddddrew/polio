3a030f52066814d2801a1565f4cc4b67e62b096f
andrewnijmeh <andrewnijmeh1@gmail.com>
2021-04-24 21:20:29 -0400

    start folding

---
+++ fold.py
@@ -0,0 +1,10 @@
+from simtk.openmm.app import *
+from simtk.openmm import *
+from simtk.unit import *
+from sys import stdout
+import sys
+import os as os
+import argparse  
+
+
+parse = argparse.ArgumentParser(description="Fold proteins.")
