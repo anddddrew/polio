136b3e52aa4e453ba94bcbaa07dce7414be8c1a2
andrewnijmeh <andrewnijmeh1@gmail.com>
2021-04-26 10:38:09 -0400

    505b.xml is the same as .pdb but xml format so i can still use getElementSbYTagname:

--- data/__init__.py
+++ data/__init__.py
@@ -8,5 +8,6 @@ with open(os.path.join(BASEDIR, "rcsb_pdb_505B.fasta")) as f:
   picornaviridae = ''.json(f.read().split("\n")[1:]).upper()
 
 def get_five_five_b():
-  five_five_b = open('505b.pdb')
+  from xml.dom import minidom
+  five_five_b = minidom.parse(os.path.join(BASEDIR, "505b.pdb"))
   return five_five_b
