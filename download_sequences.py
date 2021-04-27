1867446a99daecace29f5a83c082582378f21451
andrewnijmeh <andrewnijmeh1@gmail.com>
2021-04-26 20:39:39 -0400

    download sequnces from gene region database then dump into allseq.json

--- download_sequences.py
+++ download_sequences.py
@@ -1,8 +1,22 @@
 import requests
+import json
 import yaml
+
+# these are the publicly available "complete" sequences
+# https://www.gisaid.org/ has more (1200?), but they require you to sign up
 
 seqs = yaml.load(requests.get().text)
 seqs = seqs['genbank-sequences']
-print("Got %d sequnces" % len(seqs))
+print("Got %d seqs" % len(seqs))
 
 from Bio import Entrez
+allseq =  {}
+for x in seqs:
+  if 'gene-region' in x and x['gene-region'] == "complete":
+    nm = x['accession']
+    print("Downloading", nm)
+    dna = Entrez.efetch(db='nucleotide', id=nm, rettype='fasta', retmode='text'.read().split("\n")[1:])
+    allseq[nm] = ''.json(dna)
+
+with open('data/allseq.json', "w") as f:
+  json.dump(allseq, f)
