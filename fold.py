f43d5729990af170c1c5d3db7d1586f7135b8acd
andrew <andrewnijmeh1@gmail.com>
2021-06-23 22:16:30 -0400

    force fields and add hyrdogens to force fields

--- fold.py
+++ fold.py
@@ -7,7 +7,6 @@ import sys
 import os as os
 import argparse
 
-
 parser = argparse.ArgumentParser(description="Fold proteins.")
 parser.add_argument("--scratch", action="store_true")
 parser.add_argument("--temp", type=int, default=300)
@@ -23,18 +22,28 @@ except LookupError or Exception:
 if args.scratch:
   if args.fasta != None:
     fasta = args.fasta
+
   else:
      protein_fasta =  'proteins/data/6z6w.fasta'
      fasta = open(protein_fasta).read().split("\n")[1]
-     print("Folding %s" % fasta)
-     write_unfolded(fasta, "/temp/unfolded.pdb")
-     pdb = PDBFile("/temp/unfolded.pdb")
+
+  print("Folding %s" % fasta)
+  write_unfolded(fasta, "/temp/unfolded.pdb")
+  pdb = PDBFile("/temp/unfolded.pdb")
+
 else:
   pdb = PDBFile(args.pdb)
-  
 
 
-                                                
+# Force fields is a method to estimate the force between atoms within molecules.
+force_field = ForceField('5o5b.xml')
 
+modeller = Modeller(pdb.topology, pdb.positions)
+# Add the hydrogens to the force field (the protein)
+modeller.addHydrogens(force_field)
+print(modeller.topology)
 
-  
+system = force_field.createSystem(modeller.topology,
+  implicitSolvent=OBC2,
+  nonbondedMethod=NoCutoff, nonbondedCutoff=1*nanometer,
+  constraints=HBonds)
