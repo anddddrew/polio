f7755048ec82d933694a90b64820671391230742
andrew <andrewnijmeh1@gmail.com>
2021-09-29 15:56:41 -0400

    remove spike protein only needed in some virus

--- opt.py
+++ opt.py
@@ -3,7 +3,6 @@ from lib import genome as virus
 virus = virus.get_nuceotideSequence().replace("T", "U")
 vaccine = vaccine.replace("Ψ", "U")
 
-# We got to find the start of the vaccine and the spike of the protein and a percentage match.
 # vacine starts at "" with a % chance
 vvirus = virus[+len(vaccine)]
 
