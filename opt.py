2ea74a194efafd5e1c6a0ab50951ddba43c8109d
Andrew <andrewnijmeh1@gmail.com>
2022-03-11 20:51:19 -0500

    length

--- opt.py
+++ opt.py
@@ -1,10 +1,6 @@
 from lib import genome as virus 
 
 virus = virus.get_nuceotideSequence().replace("T", "U")
-vaccine = vaccine.replace("Ψ", "U")
 
-# vacine starts at "" with a % chance
-vvirus = virus[+len(vaccine)]
 
-print(vvirus)
-print(vaccine)
+
